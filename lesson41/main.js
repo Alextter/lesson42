804ab3b4edc8ea748732f548000bfbebccd348e0
Alextter <74066029+Alextter@users.noreply.github.com>
2021-03-29 18:55:10 +0300

    Add files via upload

---
+++ lesson41/main.js
@@ -0,0 +1,152 @@
+// let a = prompt("Какой язык?")
+// switch (a){
+//     case "ru" :
+//         alert("Вы хотите на русском");
+//     case "ger" : 
+//     alert("На немецком");
+//     case "en" : 
+//     alert("На английском");
+//     default :
+//     alert("Не поддерживает");
+// }
+
+
+
+// let a = prompt("Vvedi chislo")
+// switch (a==0){
+//     case true : 
+//     alert("Праввильно");break;
+//     default:
+//         alert("Не верно");break;
+// }
+
+// let a = Number(prompt("vvedite chislo"))
+// switch (a > 0){
+//     case true :
+//         alert("Правильно");break;
+//     default:
+//         alert(" Не правиьно");
+    
+// }
+
+
+//  let a = prompt("Vedi chislo")
+
+//  switch (a < 0){
+//      case true :
+//          alert(" prav");break;
+//      default:
+//          alert("ne prav");
+//  }
+
+// let a = prompt("Введи число")
+// switch(a >= 0){
+//    case true :
+//        alert("true");break;
+//    default:
+//        alert("folse");
+// }
+
+// let a = prompt("Введи число")
+//  switch(a <= 0){
+//     case true :
+//           alert("true");break;
+//     default:
+//             alert("folse");
+//  }
+
+// let a = prompt("Введи число")
+//  switch(a != 0){
+//     case true :
+//           alert("true");break;
+//      default:
+//             alert("folse");
+//  }
+
+// let a = prompt("Введи число")
+//   switch(a == "Test"){
+//      case true :
+//            alert("true");break;
+//      default:
+//              alert("folse");
+//   }
+
+// let a = prompt("Введи число")
+//  switch(a == 1|| a == "1"){
+//      case true :
+//            alert("true");break;
+//      default:
+//             alert("folse");
+//   }
+
+
+let a = prompt("Введи число")
+if (a = 0) {
+    alert( " true")
+}
+else{
+    alert("folse")
+}
+
+
+let a = prompt("Введи число")
+if (a > 0) {
+    alert( " true")
+}
+else{
+    alert("folse")
+}
+
+
+let a = prompt("Введи число")
+if (a < 0) {
+    alert( " true")
+}
+else{
+    alert("folse")
+}
+
+
+let a = prompt("Введи число")
+if (a >= 0) {
+    alert( " true")
+}
+else{
+    alert("folse")
+}
+
+
+let a = prompt("Введи число")
+if (a <= 0) {
+    alert( " true")
+}
+else{
+    alert("folse")
+}
+
+
+let a = prompt("Введи число")
+if (a != 0) {
+    alert( " true")
+}
+else{
+    alert("folse")
+}
+
+
+let a = prompt("Введи число")
+if (a = "test") {
+    alert( " true")
+}
+else{
+    alert("folse")
+}
+
+let a = prompt("Введи число")
+if (a = 1  || a ="1") {
+    alert( " true")
+}
+else{
+    alert("folse")
+}
+
